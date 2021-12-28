7c3e27ee0f64e1d09018d37fe552e6458a07b144
Kadir Akyar <abdulkadir.akyar@nksales.fr>
2021-12-28 16:19:08 +0100

    ✨ footer

---
+++ components/footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer container-fluid">
+    <div class="footer__content container">
+      <div class="footer__left">
+        <p>Track your ennemies</p>
+      </div>
+      <div class="footer__right">
+        <span class="active">EN</span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: #323133;
+  color: #fff;
+  height: 10rem;
+  &__content {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__right {
+    & > span {
+      opacity: 0.5;
+      font-weight: 700;
+    }
+    & .active {
+      opacity: 1;
+    }
+  }
+}
+</style>

fcaeba7a2128b683d81d790cd2c0ee4b9d872b26
Kadir Akyar <abdulkadir.akyar@nksales.fr>
2022-02-24 17:12:05 +0100

    🚧 game history

--- components/Atoms/Game.vue
+++ components/Atoms/Game.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <pre>{{ game }}</pre>
+  <div class="columns">
+    <div class="column" v-if="$fetchState.pending"></div>
+    <div class="column" v-else>
+      <p>Champion name: {{ player.championName }}</p>
+      <p>{{ player.item0 }}</p>
+      <p>{{ player.item1 }}</p>
+      <p>{{ player.item2 }}</p>
+      <p>{{ player.item3 }}</p>
+      <p>{{ player.item4 }}</p>
+      <p>{{ player.item5 }}</p>
+      <p>{{ player.item6 }}</p>
+      <pre>{{ player }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
 // Libs
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 export default {
   data() {
     return {
@@ -16,6 +27,17 @@ export default {
   props: ["gameId"],
   async fetch() {
     this.game = await this.getGame(this.gameId);
+  },
+  computed: {
+    ...mapGetters({
+      summoner: "getSummoner",
+    }),
+    player() {
+      const tab = this.game.info.participants.filter(
+        (e) => e.summonerName === this.summoner.name
+      );
+      return tab[0];
+    },
   },
   methods: {
     ...mapActions({
--- components/Molecules/Summoner/Games.vue
+++ components/Molecules/Summoner/Games.vue
@@ -1,21 +1,39 @@
 <template>
-  <div v-if="games.length">
-    <div v-for="game in games" :key="game">
-      <AtomsGame :gameId="game" />
+  <div>
+    <div v-if="$fetchState.pending">Loading ...</div>
+    <div v-else>
+      <div v-if="games.length">
+        <div v-for="gameId in games" :key="gameId">
+          <AtomsGame :gameId="gameId" />
+        </div>
+      </div>
+      <div v-else>
+        <p class="has-text-centered">No games found</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 export default {
   data() {
     return {
       games: [],
     };
   },
+  watch: {
+    async summoner() {
+      this.games = await this.getGames();
+    },
+  },
   async fetch() {
     this.games = await this.getGames();
+  },
+  computed: {
+    ...mapGetters({
+      summoner: "getSummoner",
+    }),
   },
   methods: {
     ...mapActions({
@@ -24,6 +42,3 @@ export default {
   },
 };
 </script>
-
-<style>
-</style>
